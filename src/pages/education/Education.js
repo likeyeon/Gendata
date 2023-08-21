bbb25e4271f7d2b63466f7d97af2fd05ae9689b2
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-21 20:47:38 +0900

    Chore: Add page files

---
+++ src/pages/education/Education.js
@@ -0,0 +1,11 @@
+import Gnb from "../../components/gnb/Gnb";
+
+export default function Education() {
+  return (
+    <>
+      <Gnb />
+      <br />
+      Education; Education; Education;
+    </>
+  );
+}
