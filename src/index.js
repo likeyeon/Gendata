c9b4c7ccfdaf62142d15e9dc72a5e60baefd5374
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-22 00:12:27 +0900

    Remove: index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
