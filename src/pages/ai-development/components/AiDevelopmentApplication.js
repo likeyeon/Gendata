18185d503520b3bf188c324713de1ef54ae61446
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-22 00:47:38 +0900

    Feat: Add AiDevelopment page application component

---
+++ src/pages/ai-development/components/AiDevelopmentApplication.js
@@ -0,0 +1,10 @@
+<div className={styles.applicationBackground}>
+  <h3 className={styles.applicationText}>걱정하지 마세요.</h3>
+  <div className={styles.applicationContainer}>
+    <img className={styles.applicationItems} src={card1} alt="card" />
+    <img className={styles.applicationItems} src={card2} alt="card" />
+    <img className={styles.applicationItems} src={card3} alt="card" />
+    <img className={styles.applicationItems} src={card4} alt="card" />
+  </div>
+  <img className={styles.character} src={character} alt="character" />
+</div>;
