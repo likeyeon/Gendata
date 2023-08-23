ac67265b72bc1c21b26d3584e565ef57133031dd
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-24 00:42:10 +0900

    Design: tag 변경(b -> strong)

--- src/pages/ai-development/components/AiDevlopmentTitle.js
+++ src/pages/ai-development/components/AiDevlopmentTitle.js
@@ -1,15 +1,16 @@
 import styles from "./AiDevlopmentTitle.module.scss";
-
+import "../../../style/style.scss";
 const AiDevlopmentTitle = () => {
   return (
-    <div className={styles.aiDevelopmentTitle}>
+    <div className={styles.AiDevlopmentTitle}>
       <h1 className={styles.Title}>AI 개발</h1>
       <p className={styles.Text}>
-        <b>고품질 학습 데이터를 통해</b> 학습시켜서 AI 모델을 추천하고
+        <strong>고품질 학습 데이터를 통해</strong> 학습시켜서 AI 모델을 추천하고
         개발합니다.
-        <br />
-        AI의 목적을 고려해서 구축한 <b>고품질 데이터셋</b>을 학습시키기 때문에
-        더 개선된 성능을 확보할 수 있습니다.
+      </p>
+      <p className={styles.Text}>
+        AI의 목적을 고려해서 구축한 <strong>고품질 데이터셋</strong>을
+        학습시키기 때문에 더 개선된 성능을 확보할 수 있습니다.
       </p>
     </div>
   );
