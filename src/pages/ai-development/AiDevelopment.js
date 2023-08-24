04f487cc72c4b80328d384b5737b1553c04be03b
heekyung <mkmc020@gmail.com>
2023-08-25 02:35:36 +0900

    [1M2H} Feat: Common component 'Strength(강점)' 만들기 (#19)
    * Feat: Add Common component Strength
    
    * Refactor: 컴포넌트 분리
    design: 미디어 쿼리 적용, 여백조정 미완

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -1,9 +1,11 @@
 import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
+import Strength from "./../../components/strength/Strength";
 import Contact from "./components/Contact";
 export default function AiDevelopment() {
   return (
     <>
       <AiDevlopmentTitle />
+      <Strength />
       <Contact />
     </>
   );
