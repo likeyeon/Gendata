2dccdc67530c7fcdb0aee356ffc277807da34ad5
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-25 03:20:36 +0900

    design: 폰트 및 미디어쿼리 적용

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -1,10 +1,12 @@
 import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
+import AiDevelopmentCase from "./components/AiDevelopmentCase";
 import Strength from "./../../components/strength/Strength";
 import Contact from "./components/Contact";
 export default function AiDevelopment() {
   return (
     <>
       <AiDevlopmentTitle />
+      <AiDevelopmentCase />
       <Strength />
       <Contact />
     </>
--- src/pages/ai-development/components/AiDevelopmentCase.js
+++ src/pages/ai-development/components/AiDevelopmentCase.js
@@ -1,5 +1,5 @@
 import styles from "./AiDevelopmentCase.module.scss";
-
+import { Subtitle } from "../../../components/common/typo/Typo";
 import card7 from "../../../assets/ai-development/card7.png";
 import card8 from "../../../assets/ai-development/card8.png";
 import card9 from "../../../assets/ai-development/card9.png";
@@ -7,7 +7,7 @@ import card9 from "../../../assets/ai-development/card9.png";
 const AiDevlopmentCase = () => {
   return (
     <div className={styles.aiDevlopmentCase}>
-      <h3 className={styles.aiDevlopmentCaseTitle}>활용 사례</h3>
+      <Subtitle subtitle="활용 사례" className={styles.aiDevlopmentCaseTitle} />
       <div className={styles.aiDevlopmentCaseContainer}>
         <img className={styles.aiDevlopmentCaseImages} src={card7} alt="card" />
         <img className={styles.aiDevlopmentCaseImages} src={card8} alt="card" />
