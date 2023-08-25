b57c281bf937b280830df0b3ee2c49fdafdfcad8
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-25 21:16:31 +0900

    Refactor: 무한루프 수정, Design:	미디어 쿼리 적용

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -1,20 +1,19 @@
-import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
-import AiDevelopmentApplication from "./components/AiDevelopmentApplication";
-import AiDevelopmentCase from "./components/AiDevelopmentCase";
-import Strength from "./../../components/strength/Strength";
-import Contact from "./components/Contact";
+// import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
+// import AiDevelopmentApplication from "./components/AiDevelopmentApplication";
+// import AiDevelopmentCase from "./components/AiDevelopmentCase";
+// import Strength from "./../../components/strength/Strength";
+// import Contact from "./components/Contact";
 import AiDevelopmentRecommend from "./components/AiDevelopmentRecommend";
 export default function AiDevelopment() {
   return (
     <>
       <AiDevelopmentRecommend />
-
-      <AiDevlopmentTitle />
+      {/* <AiDevlopmentTitle />
       <AiDevelopmentApplication />
       <AiDevelopmentRecommend />
       <AiDevelopmentCase />
       <Strength />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
