0d2a2d2fb6558d8752ff775e0dcf37de2737b2d3
heekyung <mkmc020@gmail.com>
2023-08-25 17:33:39 +0900

    Revert "[1M30M] Feature/heekyung/ai develop/recommend (#15)"
    This reverts commit 69cc3743b3e754977e1c49500d58ac721fae6806.

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -2,12 +2,9 @@ import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
 import AiDevelopmentApplication from "./components/AiDevelopmentApplication";
 import Strength from "./../../components/strength/Strength";
 import Contact from "./components/Contact";
-import AiDevelopmentRecommend from "./components/AiDevelopmentRecommend";
 export default function AiDevelopment() {
   return (
     <>
-      <AiDevelopmentRecommend />
-
       <AiDevlopmentTitle />
       <AiDevelopmentApplication />
       <Strength />
