0edae1a67772bf707980026e5680b8ed560ffb41
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-25 14:05:42 +0900

    fixing: flex

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -1,9 +1,12 @@
 import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
 import Strength from "./../../components/strength/Strength";
 import Contact from "./components/Contact";
+import AiDevelopmentRecommend from "./components/AiDevelopmentRecommend";
 export default function AiDevelopment() {
   return (
     <>
+      <AiDevelopmentRecommend />
+
       <AiDevlopmentTitle />
       <Strength />
       <Contact />
--- src/pages/ai-development/components/AiDevelopmentRecommend.js
+++ src/pages/ai-development/components/AiDevelopmentRecommend.js
@@ -1,19 +1,61 @@
+import React, { useState, useCallback, useEffect } from "react";
+
 import styles from "./AiDevelopmentRecommend.module.scss";
+import { Subtitle, Contents } from "../../../components/common/typo/Typo";
 
 import card5 from "../../../assets/ai-development/card5.png";
 import card6 from "../../../assets/ai-development/card6.png";
 import files from "../../../assets/ai-development/files.png";
 
 const AiDevelopmentRecommend = () => {
+  const [contentsString, setTitleString] = useState("");
+  const maxWidthTitle = (
+    <>
+      우리 회사에 적합한 AI 기술과 모델,
+      <br />
+      젠데이터에서 추천받으세요.
+    </>
+  );
+  const minWidthTitle = (
+    <>
+      우리 회사에 적합한 AI 기술과
+      <br />
+      모델, 젠데이터에서 추천받으세요.
+    </>
+  );
+
+  const tabletMql = window.matchMedia("screen and (min-width:992px)");
+  const changeEventHandler = useCallback(
+    (e) => {
+      if (e.matches) {
+        setTitleString(maxWidthTitle);
+      } else {
+        setTitleString(minWidthTitle);
+      }
+    },
+    [maxWidthTitle, minWidthTitle]
+  );
+  tabletMql.addEventListener("change", changeEventHandler);
+
+  useEffect(() => {
+    if (window.innerWidth > 992) {
+      setTitleString(maxWidthTitle);
+    }
+
+    return () => {
+      tabletMql.removeEventListener("change", changeEventHandler);
+    };
+  }, []);
+
   return (
     <div className={styles.recommendBackground}>
       <div className={styles.recommendContent}>
         <div className={styles.recommendContentText}>
-          <h2 className={styles.recommendContentTitle}>
-            우리 회사에 적합한 AI 기술과 모델,
-            <br />
-            젠데이터에서 추천받으세요.
-          </h2>
+          <Subtitle
+            className={styles.recommendContentTitle}
+            subtitle={contentsString}
+          />
+
           <p className={styles.recommendContentDescription}>
             우리 회사 비즈니스에 맞는 AI 기술을 찾고 계시나요?
           </p>
