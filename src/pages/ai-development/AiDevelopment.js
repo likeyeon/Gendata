55ab7450b4873256cf6054f2d17180db45471ea9
heekyung <mkmc020@gmail.com>
2023-08-25 17:22:13 +0900

    [1M20M]Feat: Add AiDevelopment page application component (#14)
    * Feat: Add AiDevelopment page application component
    
    * Refactor: few change
    
    * Refactor: 컴포넌트 분리
     Design:: 미디어쿼리 적용

--- src/pages/ai-development/AiDevelopment.js
+++ src/pages/ai-development/AiDevelopment.js
@@ -1,10 +1,12 @@
 import AiDevlopmentTitle from "./components/AiDevlopmentTitle";
+import AiDevelopmentApplication from "./components/AiDevelopmentApplication";
 import Strength from "./../../components/strength/Strength";
 import Contact from "./components/Contact";
 export default function AiDevelopment() {
   return (
     <>
       <AiDevlopmentTitle />
+      <AiDevelopmentApplication />
       <Strength />
       <Contact />
     </>
