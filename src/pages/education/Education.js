39eaa0b3b98d5f1680678414ccec1d79256a6331
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-26 07:59:58 +0900

    Feat: 컴포넌트, 디자인, 미디어쿼리 추가

--- src/pages/education/Education.js
+++ src/pages/education/Education.js
@@ -1,10 +1,12 @@
 import EducitionTitle from "./components/educationTitle/EducationTitle";
 import EducationIntro from "./components/educationIntro/EducationIntro";
+import EducationSystem from "./components/educationSystem/EducationSystem";
 export default function Education() {
   return (
     <>
       <EducitionTitle />
       <EducationIntro />
+      <EducationSystem />
     </>
   );
 }
