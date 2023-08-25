4ab8b5a80030123b9e179d57ee3f9de791b0c2d4
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-26 07:20:40 +0900

    Feat: 컴포넌트 제작 및 css, 미디어쿼리 적용

--- src/pages/education/Education.js
+++ src/pages/education/Education.js
@@ -1,9 +1,10 @@
 import EducitionTitle from "./components/educationTitle/EducationTitle";
-
+import EducationIntro from "./components/educationTitle/EducationIntro";
 export default function Education() {
   return (
     <>
       <EducitionTitle />
+      <EducationIntro />
     </>
   );
 }
