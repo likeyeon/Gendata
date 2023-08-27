98b6bff5d34f953031a721224c3f599c5d0fa356
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-27 21:07:07 +0900

    Feat: 함수 -> 함수 별과값 변수

--- src/pages/news/components/newsBoard/components/getBoardData.js
+++ src/pages/news/components/newsBoard/components/getBoardData.js
@@ -1,9 +1,13 @@
-export function getBoardListData(NewsBoardData) {
+import NewsBoardData from "../../../../../Mock/newsBoard.json";
+
+const getBoardListData = () => {
   const boardListData = NewsBoardData.map((item) => ({
     id: item.id,
     title: item.title,
     date: item.date,
     contents: item.contents,
   }));
-  return boardListData.reverse();
-}
+  return boardListData;
+};
+
+export const boardData = getBoardListData();
