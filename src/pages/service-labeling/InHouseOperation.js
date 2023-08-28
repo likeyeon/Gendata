c597eda1de0ae6291ff6e2117e45a582632b1b11
kjin0520 <kjin0520@gmail.com>
2023-08-28 11:26:52 +0900

    Feat : 품질 및 보안 인하우스 작업 프로세스 섹션 구현

---
+++ src/pages/service-labeling/InHouseOperation.js
@@ -0,0 +1,15 @@
+import Strength from "../../components/strength/Strength";
+import Contact from "../ai-development/components/Contact";
+import InHouseTitle from "./components/InHouseTitle";
+import InHouseWorkProcess from "./components/InHouseWorkProcess";
+
+export default function InHouseOperation() {
+  return (
+    <>
+      <InHouseTitle />
+      <InHouseWorkProcess />
+      <Strength />
+      <Contact />
+    </>
+  )
+}
