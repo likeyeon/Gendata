bba2993dff96979296da193fd4dacbffc0f08b30
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-28 21:54:37 +0900

    Design: Ai devlopment 타이틀 css 조정 완료

--- src/pages/ai-development/components/AiDevlopmentTitle.js
+++ src/pages/ai-development/components/AiDevlopmentTitle.js
@@ -1,5 +1,5 @@
 import Title from "../../../components/title/Title";
-import styles from "./AiDevlopmentTitle.module.scss";
+
 const AiDevlopmentTitle = () => {
   return (
     <div className={styles.titleContainer}>
@@ -7,13 +7,13 @@ const AiDevlopmentTitle = () => {
         title="AI 개발"
         titleClassName=""
         maxContents={
-          <div>
+          <>
             <strong>고품질 학습 데이터를 통해</strong> 학습시켜서 AI 모델을
             추천하고 개발합니다.
             <br />
             AI의 목적을 고려해서 구축한 <strong>고품질 데이터셋</strong>을
             학습시키기 때문에 더 개선된 성능을 확보할 수 있습니다.
-          </div>
+          </>
         }
         minContents={
           <>
