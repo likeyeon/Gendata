eca8e86a0470578d77e2faf92bed45235aab77d8
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-28 19:45:59 +0900

    Chore: 오타 수정

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,18 +1,13 @@
+//import React, { useState, useCallback, useEffect, useMemo } from "react";
 import styles from "./Title.module.scss";
 import { TitleBig, Contents } from "../common/typo/Typo";
 
-const Title = ({ titleClassName, maxContents, minContents }) => {
+const Title = ({ title, titleClassName, maxContents, minContents }) => {
   return (
     <div className={styles.title}>
-      <TitleBig className={titleClassName} titleBig={titleText} />
-      <Contents
-        className={`${styles.maxContents} {contentClassName}`}
-        contents={maxContents}
-      />
-      <Contents
-        className={`${styles.minContents} {contentClassName}`}
-        contents={minContents}
-      />
+      <TitleBig className={titleClassName} titleBig={title} />
+      <Contents className={`${styles.maxContents}`} contents={maxContents} />
+      <Contents className={`${styles.minContents}`} contents={minContents} />
     </div>
   );
 };
