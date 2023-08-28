967bfcab33c2bc76f935146a728fcf9f9a3f6581
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-28 18:18:51 +0900

    Desin: 수정중

--- src/pages/ai-development/components/AiDevlopmentTitle.js
+++ src/pages/ai-development/components/AiDevlopmentTitle.js
@@ -1,19 +1,19 @@
 import Title from "../../../components/title/Title";
-
+import styles from "./AiDevlopmentTitle.module.scss";
 const AiDevlopmentTitle = () => {
   return (
-    <>
+    <div className={styles.titleContainer}>
       <Title
         title="AI 개발"
         titleClassName=""
         maxContents={
-          <>
+          <div>
             <strong>고품질 학습 데이터를 통해</strong> 학습시켜서 AI 모델을
             추천하고 개발합니다.
             <br />
             AI의 목적을 고려해서 구축한 <strong>고품질 데이터셋</strong>을
             학습시키기 때문에 더 개선된 성능을 확보할 수 있습니다.
-          </>
+          </div>
         }
         minContents={
           <>
@@ -25,7 +25,7 @@ const AiDevlopmentTitle = () => {
         }
         contentClassName=""
       />
-    </>
+    </div>
   );
 };
 export default AiDevlopmentTitle;
