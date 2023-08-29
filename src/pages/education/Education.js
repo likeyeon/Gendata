586511cde509bd427107bf473bcfdf19a9da6e2d
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-08-29 18:27:34 +0900

    fix: 좌우여백

--- src/pages/education/Education.js
+++ src/pages/education/Education.js
@@ -7,11 +7,11 @@ import Contact from "./components/Contact";
 export default function Education() {
   return (
     <>
-      <EducitionTitle />
+      {/* <EducitionTitle />
       <EducationIntro />
       <EducationSystem />
       <EducationReview />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
