0b514bcf614358b421985eceaaaf1113658f92cd
likeyeon <chy98csy00@naver.com>
2023-08-30 03:50:26 +0900

    Design: 데이터 가공 반응형 css 수정

--- src/pages/data-dealing/components/DataDealingTitle.js
+++ src/pages/data-dealing/components/DataDealingTitle.js
@@ -1,13 +1,33 @@
 import Title from "../../../components/title/Title";
+import { useEffect, useState } from "react";
 
 const GovernmentTitle = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Title
         title="데이터거래"
         titleClassName="TitleBig"
         minContents={
-          <>젠데이터가 기업 간의 데이터 거래를 중개하고 매칭시켜드립니다.</>
+          windowSize > 991 ? (
+            <>젠데이터가 기업 간의 데이터 거래를 중개하고 매칭시켜드립니다.</>
+          ) : (
+            <>
+              젠데이터가 기업 간의 데이터 거래를 <br />
+              중개하고 매칭시켜드립니다.
+            </>
+          )
         }
       />
     </>
